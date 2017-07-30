202c9a111460837e5669c49e73abbcb58f3b1785
Anton Kloek <anton.kloek@gmail.com>
2017-07-30 12:28:21 +0200

    testing new reward function + amend, less punish for damage and old speed reward

--- gym_torcs.py
+++ gym_torcs.py
@@ -111,7 +111,7 @@ class TorcsEnv:
 
         # collision detection
         if obs['damage'] - obs_pre['damage'] > 0:
-            reward = -obs['damage']
+            reward = -(obs['damage'] - obs_pre['damage'])
 
         # Termination judgement #########################
         #if track.min() < 0:  # Episode is terminated if the car is out of track
@@ -122,7 +122,7 @@ class TorcsEnv:
 
         if self.terminal_judge_start < self.time_step:  # Episode terminates if the progress of agent is small
             if((progress < self.termination_limit_progress) and early_stop ):
-                #reward = -50
+                reward = -50
                 print("META = 1 ... Minimal Progress!")
                 self.client.R.effectors['meta'] = 1
 
